0decda96e2132300a428e68216a687ac9c358c91
Fukushima Takahiro <fukkuaaa@gmail.com>
2024-01-19 02:18:01 +0900

    create scrapy settings

---
+++ src/services/site_scraper/custom_spider.py
@@ -0,0 +1,47 @@
+import scrapy
+import logging
+from logging import StreamHandler, Formatter
+
+
+class CustomSpider(scrapy.Spider):
+    def __init__(self, *args, **kwargs):
+        # その他設定を継承
+        super().__init__(*args, **kwargs)
+        # 指定したハンドラが出力するログのレベルをINFOに上書き
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        # ログのフォーマットの設定
+        handler_format = Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s', datefmt='%Y/%d/%m %I:%M:%S')
+        stream_handler.setFormatter(handler_format)
+        # 各ハンドラに設定を反映
+        stat_logger = logging.getLogger('scrapy.statscollectors')
+        stat_logger.addHandler(stream_handler)
+        scrape_logger = logging.getLogger('scrapy.core.scraper')
+        scrape_logger.addHandler(stream_handler)
+        # 各spider_nameを名前に持つハンドラから、スクレイピング開始をロギングする
+        start_logger = logging.getLogger(__name__)
+        start_logger.addHandler(stream_handler)
+        start_logger.info('scrapy started')
+
+
+class CustomCrawlSpider(scrapy.spiders.CrawlSpider):
+    def __init__(self, *args, **kwargs):
+        # その他設定を継承
+        super().__init__(*args, **kwargs)
+        # 指定したハンドラが出力するログのレベルをINFOに上書き
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        # ログのフォーマットの設定
+        handler_format = Formatter(
+            '%(asctime)s [%(name)s] %(levelname)s: %(message)s', datefmt='%Y/%d/%m %I:%M:%S')
+        stream_handler.setFormatter(handler_format)
+        # 各ハンドラに設定を反映
+        stat_logger = logging.getLogger('scrapy.statscollectors')
+        stat_logger.addHandler(stream_handler)
+        scrape_logger = logging.getLogger('scrapy.core.scraper')
+        scrape_logger.addHandler(stream_handler)
+        # 各spider_nameを名前に持つハンドラから、スクレイピング開始をロギングする
+        start_logger = logging.getLogger(__name__)
+        start_logger.addHandler(stream_handler)
+        start_logger.info('scrapy started')
---
+++ src/services/site_scraper/logging_formatter.py
@@ -0,0 +1,26 @@
+from scrapy import logformatter
+import logging
+
+
+class LoggingFormatter(logformatter.LogFormatter):
+    # crowl時のLogLevelをDEBUGに落とす
+    def crawled(self, request, response, spider):
+        return {
+            'level': logging.DEBUG,
+            'msg': 'Crawled %(request)s',
+            'args': {
+                'response': response,
+                'request': request,
+            }
+        }
+
+    # scraped時のLogFormatを変更する
+    def scraped(self, item, response, spider):
+        return {
+            'level': logging.INFO,
+            'msg': 'Scraped from %(response)s success',
+            'args': {
+                'item': item,
+                'response': response,
+            }
+        }
